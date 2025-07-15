d1c18e515647af446bd43d8e77920b9ec67a1339
Joseph Kwabena Osei Bonsu <josephoseibonsu742@gmail.com>
2025-07-15 11:59:11 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
-const ytdl = require('ytdl-core);
-const ffmpeg = require('ffmpeg-fluent');
+const ytdl = require('ytdl-cor'e);
+const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 require('dotenv').config();
 
